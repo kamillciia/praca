1b01bba91c6c16790876c8e3bc87f14a5cfeae4b
kamillciia <32923520+kamillciia@users.noreply.github.com>
2018-01-08 11:02:06 +0100

    Create nwd.c

---
+++ nwd.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+int NWD (int a, int b)
+{
+	int temp;
+	while (b != 0)
+	{
+		temp = b;
+		b = a % b;
+		a = temp;
+			
+	}
+	printf("NWD podanych liczb wynosi: %d", a );
+}
+
+int main()
+{
+	int a, b, temp;
+	printf("Podaj liczbe a: \n");
+	scanf("%d", &a);
+	printf("Podaj liczbe b: \n");
+	scanf("%d", &b);
+	NWD(a,b);
+	
+	return 0;
+}
