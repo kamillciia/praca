2514b80801f4fe4ad000b22e1692563c11380782
kamillciia <32923520+kamillciia@users.noreply.github.com>
2018-02-07 12:08:23 +0100

    Create nwd_rekurencje.c

---
+++ nwd_rekurencje.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int rek(int x,int y)
+{
+    if(x==y);
+
+    else if(x>y) return rek(x-y,y);
+
+    else return rek(x,y-x);
+}
+
+int main()
+{
+
+    int a, b ;
+    printf("Podaj liczbe calkowita a>0\n");
+    scanf("%d", &a);
+    printf("Podaj liczbe calkowita b>0\n");
+    scanf("%d", &b);
+    printf("Najwiekszy Wspolny dzielnik liczb:%d i %d = ", a, b, rek(a,b));
+
+    while ( b != a)
+
+        if ( b > a)
+            b = b - a;
+        else
+            a = a - b;
+    printf("%d\n", a);
+    return 0;
+
+}
